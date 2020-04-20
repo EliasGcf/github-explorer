3e746bb22784617abacfc7d4f5e23854b03c6b0a
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-04-20 14:19:46 -0300

    Updates

---
+++ precache-manifest.809e68385bd364387d80b38c37ea58b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7ad1954062be68ac05707d758697de",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "12f3477fbe9f3d168ac8",
+    "url": "/github-explorer/static/js/2.8a2a4937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.8a2a4937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190c041926921ba6bc16",
+    "url": "/github-explorer/static/js/main.526324b3.chunk.js"
+  },
+  {
+    "revision": "e2f0772bece6068ae40c",
+    "url": "/github-explorer/static/js/runtime-main.906dbdf0.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
